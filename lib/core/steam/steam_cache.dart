7830e6f1fdc4f7596ce09a2da69693ca01bc184d
James Leahy <defuncart@gmail.com>
2024-02-18 08:39:43 +0100

    SteamGridCache ensure only valid game ids are returned

--- lib/core/steam/steam_cache.dart
+++ lib/core/steam/steam_cache.dart
@@ -14,11 +14,14 @@ class SteamGridCache {
       final contents = (await dir.list().toList()).whereType<File>().where((file) => file.isImage).toList();
       contents.sort((a, b) => p.basename(a.path).compareTo(p.basename(b.path)));
 
-      final idRegEx = RegExp(r'(\d{10})');
+      const steamAppIdLength = 10;
+      // match group0 1234567890p, group1 1234567890 group2 0
+      final idRegEx = RegExp(r'^(\d{10})(p||_hero||_logo||_icon)*$');
 
+      // create a map of files whose key is appId
       final mapped = groupBy(
         contents,
-        (file) => idRegEx.firstMatch(p.basenameWithoutExtension(file.path))?.group(0) ?? '',
+        (file) => idRegEx.firstMatch(p.basenameWithoutExtension(file.path))?.group(1) ?? '',
       );
       mapped.removeWhere((key, value) => key.isEmpty);
 
@@ -26,7 +29,9 @@ class SteamGridCache {
       for (final kvp in mapped.entries) {
         final icon = kvp.value.firstWhereOrNull((file) => p.basenameWithoutExtension(file.path).contains('_icon'));
         final cover = kvp.value.firstWhereOrNull((file) => p.basenameWithoutExtension(file.path).contains('p'));
-        final background = kvp.value.firstWhereOrNull((file) => p.basenameWithoutExtension(file.path).length == 10);
+        final background = kvp.value.firstWhereOrNull(
+          (file) => p.basenameWithoutExtension(file.path).length == steamAppIdLength,
+        );
         final logo = kvp.value.firstWhereOrNull((file) => p.basenameWithoutExtension(file.path).contains('_logo'));
         final hero = kvp.value.firstWhereOrNull((file) => p.basenameWithoutExtension(file.path).contains('_hero'));
         programs.add((
