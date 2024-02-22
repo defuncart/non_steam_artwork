11dd96dec774ed68adff5bb52e49b07b301da5c5
James Leahy <defuncart@gmail.com>
2024-02-22 20:57:45 +0100

    Log device, app info

--- lib/features/splash/splash_state.dart
+++ lib/features/splash/splash_state.dart
@@ -1,4 +1,8 @@
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
+import 'package:non_steam_artwork/core/logging/logger.dart';
 import 'package:non_steam_artwork/core/steam/state.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'splash_state.g.dart';
@@ -6,5 +10,18 @@ part 'splash_state.g.dart';
 @riverpod
 Future<bool> splashState(SplashStateRef ref) async {
   await ref.read(steamManagerProvider).init();
+
+  final deviceInfo = await DeviceInfoPlugin().deviceInfo;
+  final packageInfo = await PackageInfo.fromPlatform();
+
+  ref.log(packageInfo.toString());
+  ref.log(
+    switch (defaultTargetPlatform) {
+      // LinuxDeviceInfo does not override toString
+      TargetPlatform.linux => 'DeviceInfo ${deviceInfo.data.toString()}',
+      _ => deviceInfo.toString(),
+    },
+  );
+
   return true;
 }
