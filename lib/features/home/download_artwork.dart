3e42b7c07ab346d15b8f89504b9ecfa5e40adac5
James Leahy <defuncart@gmail.com>
2024-03-01 19:02:11 +0100

    Refactor DownloadArtwork as page

--- lib/features/home/download_artwork.dart
+++ lib/features/home/download_artwork.dart
@@ -4,7 +4,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gap/gap.dart';
 import 'package:non_steam_artwork/core/l10n/l10n_extension.dart';
 import 'package:non_steam_artwork/core/steam/steam_program.dart';
 import 'package:non_steam_artwork/features/home/home_state.dart';
@@ -25,53 +24,56 @@ class DownloadArtwork extends ConsumerWidget {
     final provider = gameArtworkDownloadProvider(searchTerm: program.appName, artType: artType);
     final state = ref.watch(provider);
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        IconButton(
-          padding: EdgeInsets.zero,
-          iconSize: kMinInteractiveDimension,
-          onPressed: () => Navigator.of(context).pop(),
-          icon: const Icon(Icons.close),
-        ),
-        Expanded(
-          child: switch (state) {
-            AsyncData(:final value) => ArtworkSelector(
-                downloadableArtworks: value,
-                onSelect: (file) {
-                  ref.read(
-                    createArtworkFileProvider(
-                      appId: program.appId,
-                      file: file,
-                      ext: '.png',
-                      artType: artType,
-                    ),
-                  );
-
-                  Navigator.of(context).pop();
-                },
-              ),
-            AsyncLoading() => const Center(
-                child: CircularProgressIndicator(),
-              ),
-            AsyncError(:final error) => Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(error.toString()),
-                    TextButton(
-                      onPressed: () => ref.invalidate(provider),
-                      child: Text(
-                        context.l10n.markdownScreenErrorTryAgain,
+    return Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          IconButton(
+            padding: EdgeInsets.zero,
+            iconSize: kMinInteractiveDimension,
+            onPressed: () => Navigator.of(context).pop(),
+            icon: const Icon(Icons.close),
+          ),
+          Expanded(
+            child: switch (state) {
+              AsyncData(:final value) => ArtworkSelector(
+                  artType: artType,
+                  downloadableArtworks: value,
+                  onSelect: (file) {
+                    ref.read(
+                      createArtworkFileProvider(
+                        appId: program.appId,
+                        file: file,
+                        ext: '.png',
+                        artType: artType,
                       ),
-                    ),
-                  ],
+                    );
+
+                    Navigator.of(context).pop();
+                  },
                 ),
-              ),
-          },
-        ),
-      ],
+              AsyncLoading() => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              AsyncError(:final error) => Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(error.toString()),
+                      TextButton(
+                        onPressed: () => ref.invalidate(provider),
+                        child: Text(
+                          context.l10n.markdownScreenErrorTryAgain,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            },
+          ),
+        ],
+      ),
     );
   }
 
@@ -80,29 +82,25 @@ class DownloadArtwork extends ConsumerWidget {
     required SteamProgram program,
     required SteamGridArtType artType,
   }) =>
-      showDialog(
-        context: context,
-        builder: (context) => Material(
-          color: Colors.transparent,
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: DownloadArtwork(
-              program: program,
-              artType: artType,
-            ),
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => DownloadArtwork(
+            program: program,
+            artType: artType,
           ),
         ),
-        barrierDismissible: false,
       );
 }
 
 class ArtworkSelector extends StatefulWidget {
   const ArtworkSelector({
+    required this.artType,
     required this.downloadableArtworks,
     required this.onSelect,
     super.key,
   });
 
+  final SteamGridArtType artType;
   final Iterable<DownloadableArtwork> downloadableArtworks;
   final void Function(File) onSelect;
 
@@ -111,77 +109,79 @@ class ArtworkSelector extends StatefulWidget {
 }
 
 class _ArtworkSelectorState extends State<ArtworkSelector> {
-  var _index = 0;
   final _cacheManager = DefaultCacheManager();
-
-  int get _total => widget.downloadableArtworks.length;
-  DownloadableArtwork get _current => widget.downloadableArtworks.toList()[_index];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        const Gap(16),
-        Expanded(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              CachedNetworkImage(
-                cacheManager: _cacheManager,
-                imageUrl: _current.thumbnail,
-                imageBuilder: (context, imageProvider) => Image(
-                  image: imageProvider,
-                ),
-                progressIndicatorBuilder: (context, url, downloadProgress) => Center(
-                  child: CircularProgressIndicator(
-                    value: downloadProgress.progress,
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: double.infinity,
+        child: Center(
+          child: Wrap(
+            spacing: 8,
+            runSpacing: 8,
+            children: widget.downloadableArtworks
+                .map(
+                  (artwork) => GestureDetector(
+                    onTap: () async {
+                      final file = await _cacheManager.getSingleFile(artwork.url);
+                      widget.onSelect(file);
+                    },
+                    child: HoverableWidget(
+                      child: SizedBox.fromSize(
+                        size: widget.artType.size * 0.5,
+                        child: CachedNetworkImage(
+                          cacheManager: _cacheManager,
+                          imageUrl: artwork.thumbnail,
+                          imageBuilder: (context, imageProvider) => Image(
+                            image: imageProvider,
+                          ),
+                          progressIndicatorBuilder: (context, url, downloadProgress) => Center(
+                            child: CircularProgressIndicator(
+                              value: downloadProgress.progress,
+                            ),
+                          ),
+                          fadeInDuration: Duration.zero,
+                          fadeOutDuration: Duration.zero,
+                          errorWidget: (context, url, error) => const Icon(Icons.error),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                fadeInDuration: Duration.zero,
-                fadeOutDuration: Duration.zero,
-                errorWidget: (context, url, error) => const Icon(Icons.error),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    iconSize: kMinInteractiveDimension,
-                    splashColor: Colors.transparent,
-                    hoverColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    onPressed: _index > 0 ? () => setState(() => _index--) : null,
-                    icon: const Icon(Icons.arrow_left),
-                  ),
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    iconSize: kMinInteractiveDimension,
-                    splashColor: Colors.transparent,
-                    hoverColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    onPressed: _index < _total - 1 ? () => setState(() => _index++) : null,
-                    icon: const Icon(Icons.arrow_right),
-                  ),
-                ],
-              ),
-            ],
+                )
+                .toList(),
           ),
         ),
-        const Gap(16),
-        Center(
-          child: IconButton(
-            padding: EdgeInsets.zero,
-            iconSize: kMinInteractiveDimension,
-            onPressed: () async {
-              final file = await _cacheManager.getSingleFile(_current.url);
-              widget.onSelect(file);
-            },
-            icon: const Icon(Icons.check),
-          ),
-        ),
-      ],
+      ),
+    );
+  }
+}
+
+class HoverableWidget extends StatefulWidget {
+  const HoverableWidget({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  State<HoverableWidget> createState() => _HoverableWidgetState();
+}
+
+class _HoverableWidgetState extends State<HoverableWidget> {
+  var _isHovering = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovering = true),
+      onExit: (_) => setState(() => _isHovering = false),
+      cursor: SystemMouseCursors.click,
+      child: Opacity(
+        opacity: _isHovering ? 0.6 : 1,
+        child: widget.child,
+      ),
     );
   }
 }
