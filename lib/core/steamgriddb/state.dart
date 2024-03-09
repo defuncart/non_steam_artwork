eab6019af08ebeae907574c322d30b4b67bd5df8
James Leahy <defuncart@gmail.com>
2024-03-09 13:06:36 +0100

    Remove dio_cache_interceptor

--- lib/core/steamgriddb/state.dart
+++ lib/core/steamgriddb/state.dart
@@ -1,5 +1,3 @@
-import 'package:dio/dio.dart';
-import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:non_steam_artwork/core/settings/state.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:steamgriddb/steamgriddb.dart';
@@ -9,17 +7,6 @@ part 'state.g.dart';
 @riverpod
 SteamGridDB steamGridDBClient(SteamGridDBClientRef ref) {
   final apiKey = ref.watch(steamGridDBApiKeyControllerProvider);
-  // TODO test
-  final dio = Dio()
-    ..interceptors.add(
-      DioCacheInterceptor(
-        options: CacheOptions(
-          store: MemCacheStore(),
-          policy: CachePolicy.request,
-          maxStale: const Duration(days: 7),
-        ),
-      ),
-    );
 
-  return SteamGridDB(apiKey ?? '', dio: dio);
+  return SteamGridDB(apiKey ?? '');
 }
