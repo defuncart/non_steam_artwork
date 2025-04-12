f578fddffdfbd7252bd2281f264a355675fea038
James Leahy <defuncart@gmail.com>
2025-04-12 09:29:18 +0200

    fix: hero size

--- lib/features/home/steam_grid_art_type.dart
+++ lib/features/home/steam_grid_art_type.dart
@@ -5,7 +5,7 @@ enum SteamGridArtType {
   cover(Size(600, 900)),
   background(Size(1290, 620)),
   logo(Size(650, 248)),
-  hero(Size(1290, 620));
+  hero(Size(920, 430));
 
   const SteamGridArtType(this.size);
 
